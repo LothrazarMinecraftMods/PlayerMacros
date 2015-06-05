5921027ee3ee99fff37fcdc3cfcc15b9893f764f
sam bassett <samson.bassett@gmail.com>
2015-06-05 09:18:19 -0700

    make naming schemes consistent

--- src/main/java/com/lothrazar/samskeys/ModKeyMacros.java
+++ src/main/java/com/lothrazar/samskeys/ModKeyMacros.java
@@ -51,7 +51,7 @@ public class ModKeyMacros
 	public static final String MODID = "samskeys"; 
 	public static final String TEXTURE_LOCATION = MODID + ":";
 	public static final String VERSION = "1.8-1.0.0";
-	public static final String NAME = "Builder's Macros";
+	public static final String NAME = "Sam's Keys and Macros";
 	@Instance(value = MODID)
 	public static ModKeyMacros instance;
 	@SidedProxy(clientSide="com.lothrazar.samskeys.proxy.ClientProxy", serverSide="com.lothrazar.samskeys.proxy.CommonProxy")
