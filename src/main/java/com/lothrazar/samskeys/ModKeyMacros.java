a0f44a6387d215a7db6774f9a6e4a6ece6e4bae4
sam bassett <samson.bassett@gmail.com>
2015-06-10 10:11:38 -0700

    clean and fix mc info files. remove dead or nonfitting features

--- src/main/java/com/lothrazar/samskeys/ModKeyMacros.java
+++ src/main/java/com/lothrazar/samskeys/ModKeyMacros.java
@@ -34,13 +34,11 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
   
-@Mod(modid = ModKeyMacros.MODID, version = ModKeyMacros.VERSION,	name = ModKeyMacros.NAME, useMetadata = true )  
+@Mod(modid = ModKeyMacros.MODID, useMetadata = true )  
 public class ModKeyMacros
 {
 	public static final String MODID = "samskeys"; 
 	public static final String TEXTURE_LOCATION = MODID + ":";
-	public static final String VERSION = "1.8-1.0.0";
-	public static final String NAME = "Sam's Keys and Macros";
 	@Instance(value = MODID)
 	public static ModKeyMacros instance;
 	@SidedProxy(clientSide="com.lothrazar.samskeys.proxy.ClientProxy", serverSide="com.lothrazar.samskeys.proxy.CommonProxy")
